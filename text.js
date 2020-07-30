970037912faa1e9c93ebfe3f0cebc53616517faf
dly-47 <deanlee047@gmail.com>
2020-07-30 18:53:38 +0700

    Update text.js

--- text.js
+++ text.js
@@ -3,13 +3,18 @@ function circularText(txt, radius, classIndex) {
       classIndex = document.getElementsByClassName("circTxt")[classIndex];
   
     var deg = 180 / txt.length,
-      origin = -90;
-  
-    txt.forEach((ea) => {
-      ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>${ea}</p>`;
+    origin = -90;
+    //   txt.forEach((ea) => {
+    //     ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>${ea}</p>`; ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>${ea}</p>`;
+    //     classIndex.innerHTML += ea;
+    //     origin += deg;
+    // });
+    var i;
+    for(i=0; i<txt.length; i++){
+      ea = "<p style='height:"+radius+"px;position:absolute;transform:rotate("+origin+"deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>"+txt[i]+"</p>";
       classIndex.innerHTML += ea;
       origin += deg;
-    });
+    }
   }
   
   circularText("Online telefooncentrale", 105, 0);
