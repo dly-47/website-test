9c7269a5011da8fb87e6659e3bb82f02489ea07c
dly-47 <deanlee047@gmail.com>
2020-08-07 15:55:33 +0700

    fixed-slider

--- slider.js
+++ slider.js
@@ -1,15 +1,19 @@
-var sliders = document.getElementById("trigger");
+var slider = document.getElementById("trigger");
 var slideIndex = 1;
 var timer;
+var auto = true;
 
 showSlides(slideIndex);
-autoSlides();
 
 function plusSlides(n) {
+  auto = false;
+  clearTimeout( timer );
   showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
+  auto = false;
+  clearTimeout( timer );
   showSlides(slideIndex = n);
 }
 
@@ -19,48 +23,25 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) slideIndex = 1;
     if (n < 1) slideIndex = slides.length;
-    // for (i = 0; i < slides.length; i++) {
-    //     slides[i].style.display = "none";
-    // }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].style.backgroundColor = "white";
-    // }
     for (i = 0; i < slides.length; i++) {
         slides[i].classList.remove('slide-active');
         dots[i].classList.remove('dot-active');
     }
-    if (slideIndex > slides.length) slideIndex = 1;
-    // slides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].style.backgroundColor = "#0d9247";
-    slides[slideIndex-1].className += ' slide-active';
-    dots[slideIndex-1].className += ' dot-active';
-}
 
-function autoSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
-    // for (i = 0; i < slides.length; i++) {
-    //     slides[i].style.display = "none";
-    // }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].style.backgroundColor = "white";
-    // }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('slide-active');
-        dots[i].classList.remove('dot-active');
-    }
-    slideIndex++;
     if (slideIndex > slides.length) slideIndex = 1;
     slides[slideIndex-1].className += ' slide-active';
     dots[slideIndex-1].className += ' dot-active';
-    timer = setTimeout(autoSlides,2000);
+    if(auto)
+      slideIndex++;
+    timer = setTimeout(showSlides,2000);
 }
 
-sliders.onmouseover = function() {
+slider.onmouseover = function() {
+    auto = false;
     clearTimeout( timer );
 }
 
-sliders.onmouseout = function() {
-    timer = setTimeout( autoSlides, 2000 );
+slider.onmouseout = function() {
+    auto = true;
+    timer = setTimeout( showSlides, 2000 );
 };
--- text.js
+++ text.js
@@ -9,25 +9,12 @@ function circularText(txt, radius, classIndex) {
     txt = txt.split("");
     document.getElementsByClassName("circTxt")[classIndex].innerHTML="";
     classIndex = document.getElementsByClassName("circTxt")[classIndex];
-    
-  
     var deg = 180 / txt.length,
     origin = -90;
-    //   txt.forEach((ea) => {
-    //     ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>${ea}</p>`; ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>${ea}</p>`;
-    //     classIndex.innerHTML += ea;
-    //     origin += deg;
-    // });
     var i;
     for(i=0; i<txt.length; i++){
-      // ea = "<p style='height:"+radius+"px;position:absolute;transform:rotate("+origin+"deg);transform-origin:0 100%;left:50%;bottom:50%;font-size: 1.3em;font-weight:bold'>"+txt[i]+"</p>";
       ea = "<p class='curved' style='transform:rotate("+origin+"deg)'>"+txt[i]+"</p>";
       classIndex.innerHTML += ea;
       origin += deg;
     }
   }
-  
-  // circularText("Online telefooncentrale", 105, 0);
-  // circularText("Fysieke telefooncentrale", 105, 1);
-  // circularText("Toestellen, headsets & softphones", 105, 2);
-  // circularText("Maandabonnementen voip", 105, 3);
